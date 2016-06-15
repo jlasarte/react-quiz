5d6e7b160fc86ab07566fd1a5d2a0eb7fc14ae2e
Federico Bernardi <federico.bernardi@globant.com>
2016-06-15 17:06:36 -0300

    Listening to jslint

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
  
-import {LogIn} from './components/logInComponent';
+import { LogIn } from './components/logInComponent';
  
  
 ReactDOM.render(
-	<LogIn /> ,
-	document.getElementById("app")
+  <LogIn /> ,
+  document.getElementById('app')
 );
