73921e4ce4d9cc34f3f0302d223c730302185f64
José Giraldo <Jose.X.Giraldo.-ND@disney.com>
2016-06-16 17:29:56 -0300

    changes for try to build files in heroku

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app'
+//import { LogIn } from './components/logInComponent';
+ 
+ 
+/*ReactDOM.render(
+  <LogIn /> ,
+  document.getElementById('app')
+);
+*/
+render(<App/>, document.getElementById('app'))
