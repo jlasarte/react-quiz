d7e47fa068c5651b72e296f7d69a3d6a6b89e95e
fcmsoft <flavia_moyano@yahoo.com.ar>
2016-06-16 13:58:16 -0300

    some styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ export default React.createClass({
   render: function() {
     return (
       <div className='header navbar navbar-default'>
-        <div class="navbar-header">
-          <a class="navbar-brand" href="#"><h1> {this.props.appName} </h1></a>
+        <div className='navbar-header'>
+          <a className='navbar-brand' href='#'> <h1 className='navbar-brand-name'>{this.props.appName}</h1></a>
         </div>
       </div>
     );
