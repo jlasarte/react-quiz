b36e75c0bda2f3aaabaa3657e457cfe71b6f4af7
flavmoya <Flavia.X.Moyano.-ND@disney.com>
2016-06-21 12:32:51 -0300

    change for dev server config

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,38 +1,46 @@
-const path = require('path')
-const webpack = require('webpack')
-
+var webpack = require('webpack');
+var path = require('path');
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'src');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 module.exports = {
-  devtool: 'eval',
-
   entry: [
-    'webpack-hot-middleware/client',
-    './src/index'
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './src/index.jsx'
+    //APP_PATH,
   ],
-
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: 'bundle.js',
-    publicPath: '/build/'
-  },
-
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-
   module: {
     loaders: [
-      { test: /\.js?$/,
-        loader: 'babel',
-        exclude: path.join(__dirname, 'node_modules') },
-      { test: /\.scss?$/,
-        loader: 'style!css!sass',
-        include: path.join(__dirname, 'src', 'styles') },
-      { test: /\.png$/,
-        loader: 'file' },
-      { test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-        loader: 'file'}
+        {
+            test: /\.css$/,
+            loaders: ['style', 'css'],
+            include: APP_PATH
+        },
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel'
+        },
+        { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
     ]
-  }
-}
-
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+     path: BUILD_PATH,
+     publicPath: '/',
+     filename: 'bundle.js'
+  },
+  devServer: {
+      contentBase: './build',
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      progress: true
+    },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
